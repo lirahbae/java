731fc5d48c78e3565d8ce64601e10e93a81d1ec6
lirahbae <lirahbae@gmail.com>
2020-01-06 19:39:49 +0900

    비만등급 판별기능 추가
    +소수점 1자리만 출력하는 포맷 추가(반올림)

--- BMI/BMI.java
+++ BMI/BMI.java
@@ -2,31 +2,54 @@ import java.util.Scanner;
 public class BMI {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
 		
-		// 변수 정의 bmi, 몸무게, 신장
+		// 변수 선언
 		String name;
-		double bmi;
-		float weight;
 		float height_cm;
-		double height_m;
+		double height_m;  
+		float weight;
+		double bmi;
+		String obesity;
 		
-		//입.출력
-		System.out.println("<BMI 계산기_체질량 지수>");
-		System.out.print("이름을 입력해주세요.");
+		//사용자 입력
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("[BMI 계산기]");
+		System.out.print("이름 : ");
 		name = keyboard.next();
-		System.out.print("몸무게(kg) 를 입력해주세요.");
-		weight = keyboard.nextFloat();
-		System.out.print("신장(cm) 을 입력해주세요.");
+		System.out.print("신장 cm : ");
 		height_cm = keyboard.nextFloat();
 		height_m = height_cm * 0.01;
-		bmi = weight / (height_m * height_m);
+		System.out.print("몸무게 kg : ");
+		weight = keyboard.nextFloat();
+		keyboard.close();
+		
+		//체질량지수 계산
+		bmi = weight / (height_m * height_m);  //BMI지수 = 몸무게 kg ÷ (키 m × 키 m)
+		
+		// if조건문을 활용한 BMI지수별 비만등급
+		if (bmi < 18.5) {
+			obesity = "저체중";
+		}
+		else if (18.5 <= bmi && bmi < 23) {
+			obesity = "정상체중";
+		}
+		else if (23 <= bmi && bmi < 25) {
+			obesity = "과체중";
+		}
+		else if (25 <= bmi && bmi < 30) {
+			obesity = "비만";
+		}
+		else {
+			obesity = "고도비만";
+		}
 		
 		//결과
-		System.out.println("<결과>");
-		System.out.println("이름:" + name);
-		System.out.println("BMI지수: " + bmi);
-		keyboard.close();
+		System.out.println("[결과]");
+		System.out.println("이름 : " + name);
+		//String.format("%.1f", 변수) 는 소수점 2번째 자리에서 반올림.
+		System.out.print("BMI 지수는 " + String.format("%.1f", bmi) + ", ");
+		System.out.println(obesity + "입니다.");
+		System.out.println("(BMI 지수는 키와 몸무게만을 이용해 계산한 체질량지수로, 근육량을 고려하지 않은 수치입니다.)");
 		
 	}
 
