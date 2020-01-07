2f99397941caec703d65bc170477f0c770251976
lirahbae <lirahbae@gmail.com>
2020-01-07 23:36:34 +0900

    Create ModularArithmetic.java

---
+++ ModularArithmetic/ModularArithmetic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ModularArithmetic {
+
+	public static void main(String[] args) {
+		
+		//변수 선언
+		int a;
+		int b;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		a = keyboard.nextInt();
+		b = keyboard.nextInt();
+
+	}
+
+}
