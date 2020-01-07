6a9b5b859a3eba8131a32b2e7b037b805df27fbf
lirahbae <lirahbae@gmail.com>
2020-01-08 02:20:39 +0900

    do while반복문 이용.
    정수를 잘못 입력할 때 나오는 출력문이 앞 문장과 똑같을 때,  do while을 이용한다.
    why? 반복적인 코딩을 줄이기 위해

--- ModularArithmetic/ModularArithmetic.java
+++ ModularArithmetic/ModularArithmetic.java
@@ -8,7 +8,7 @@ public class ModularArithmetic {
 		int a; //사용자에게 받을 정수
 		int quotient;  //몫 : 10의 자리
 		int remainder;  //나머지 : 1의 자리
-		boolean isValid;
+		boolean isValid; //값이 유효한가? 에 대한 T/F값
 
 		System.out.println("[십과 일의 자리수 비교]");
 		
@@ -22,7 +22,7 @@ public class ModularArithmetic {
 		}
 		keyboard.close();
 
-//		//사용자 입력 방법 2. boolean 변수를 사용해 값이 유효한지 판별
+		//사용자 입력 방법 2. boolean 변수를 사용해 값이 유효한지 판별
 //		Scanner keyboard = new Scanner(System.in);
 //		a = keyboard.nextInt();
 //		isValid = (a>=10)&&(a<=99);
@@ -33,7 +33,25 @@ public class ModularArithmetic {
 //		}
 //		keyboard.close();
 		
-
+		
+		//사용자 입력 방법 3. do while반복문을 이용. 
+//		WHY? 방법1처럼 '2자리 정수 입력하라'는 반복적인 코딩을 줄이기 위해서
+//		Scanner keyboard = new Scanner(System.in);
+//		do {
+//			System.out.print("2자리 정수 입력 (10~99) >> ");
+//			a = keyboard.nextInt();
+//		} while ((a < 10)||(a > 99));
+//		keyboard.close();
+//		
+		
+//		<작성법>
+//		while (/*체크할조건문*/) {
+//			//while조건문에 맞으면 반복할 것들..
+//		}
+//		do {
+//			//우선 한번 실행하고, while에서 체크후 다시 반복시킬 것들...
+//		} while (/*체크할조건문*/);
+		
 		
 		//10의 자리와 1의 자리 구하기
 		quotient = a / 10;  // 58 / 10 = 5로 몫만 나옴.
@@ -47,7 +65,7 @@ public class ModularArithmetic {
 			System.out.println("NO! 10의 자리와 1의 자리가 다릅니다.");
 		}
 		
-		
+		// while 반복문 예시
 //		int x = 0;
 //		while (x < 5) {
 //			System.out.println(x);
