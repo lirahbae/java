2c3ec117ad8e1d54b230246a014dc03937422cc1
lirahbae <lirahbae@gmail.com>
2020-01-08 01:07:01 +0900

    사용자 입력에 while 반복문 추가

--- ModularArithmetic/ModularArithmetic.java
+++ ModularArithmetic/ModularArithmetic.java
@@ -8,12 +8,18 @@ public class ModularArithmetic {
 		int a; //사용자에게 받을 정수
 		int quotient;  //몫 : 10의 자리
 		int remainder;  //나머지 : 1의 자리
+
+		System.out.println("[십과 일의 자리수 비교]");
+		System.out.print("2자리 정수 입력 (10~99) >> ");
 		
 		//사용자 입력 (정수)
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("[십과 일의 자리수 비교]");
-		System.out.print("2자리 정수 입력 (10~99) >> ");
 		a = keyboard.nextInt();
+		while ((a < 10)||(a > 99)) { // while ( !((a>=10)&&(a<=99))
+			System.out.println("잘못 입력했습니다.");
+			System.out.print("10~99 사이의 숫자를 입력하세요. >> ");
+			a = keyboard.nextInt();
+		}
 		keyboard.close();
 
 		//10의 자리와 1의 자리 구하기
@@ -27,6 +33,14 @@ public class ModularArithmetic {
 		else {
 			System.out.println("NO! 10의 자리와 1의 자리가 다릅니다.");
 		}
+		
+		
+//		int x = 0;
+//		while (x < 5) {
+//			System.out.println(x);
+//			x = x + 1;
+//		}
+//		System.out.println("while반복문 끝! 현재 x:" + x);
 
 	}
 
