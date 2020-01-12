9ae762e6b4a8b38d2e03f826c7146b3447749fa2
lirahbae <lirahbae@gmail.com>
2020-01-12 22:08:02 +0900

    추가. for 반복문기능. 텅! 출력
    <Tasks 2-a>
    for 반복문 이용
    음료 개수에 맞춰 "텅!" 출력하기

--- BandingMachine/BandingMachine.java
+++ BandingMachine/BandingMachine.java
@@ -18,14 +18,23 @@ public class BandingMachine {
 		System.out.print("오백원 개수 >> ");
 		fiveHundred = keyboard.nextInt();
 		money = thousand * 1000 + fiveHundred * 500;
-		
 		System.out.println(money + "원 받았습니다." );
+	
+		//음료수 개수 입력
 		System.out.print("음료수(" + drinkcost + "원) " + "몇 개 드릴까요?");
 		drinknumber = keyboard.nextInt();
+		
+		//음료수 가격 출력
 		totalcost = drinknumber * drinkcost;
 		System.out.println("음료수 " + drinknumber + "개 " + totalcost + " 원 입니다.");
+		
+		//for문: 음료수 개수에 따라 텅! 반복출력
+		for (int i = 0; i < drinknumber; i++) {
+			System.out.print("텅!");
+		}
+		//잔돈 출력
 		change = money - totalcost;
-		System.out.println("잔돈은 " + change + "원입니다." );
+		System.out.println("\n잔돈은 " + change + "원입니다." );
 		keyboard.close();
 	}
 
