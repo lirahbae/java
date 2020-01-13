4fdfa233be0024e866f05ff584b5b29f1b18df25
lirahbae <lirahbae@gmail.com>
2020-01-14 00:25:34 +0900

    if문 심화: 추가 금액에 대한 변수추가

--- BandingMachine/BandingMachine.java
+++ BandingMachine/BandingMachine.java
@@ -11,6 +11,9 @@ public class BandingMachine {
 		int drinknumber;
 		int drinkcost = 800;
 		int totalcost;
+		int extraThousand;
+		int extraFiveHundred;
+		int extraMoney = 0;
 		
 		System.out.println("★음료수 자판기 Banding Machine★");
 		System.out.print("천원 개수 >> ");
@@ -31,16 +34,22 @@ public class BandingMachine {
 		//if문:잔돈 부족하면 메세지 출력
 		if (money < totalcost) {
 			System.out.println(totalcost - money + "원이 모자라네요.");
+			System.out.print("천원 개수 >> ");
+			extraThousand = keyboard.nextInt();
+			System.out.print("오백원 개수 >> ");
+			extraFiveHundred = keyboard.nextInt();
+			extraMoney = extraThousand * 1000 + extraFiveHundred * 500;
+			System.out.println(extraMoney + "원 더해 총 " + (money + extraMoney) + "원 받았습니다.");
+			
 		}
-		else {
-			//for문: 음료수 개수에 따라 텅! 반복출력
-			for (int i = 0; i < drinknumber; i++) {
-				System.out.print("텅!");
-			}
-			//잔돈 출력
-			change = money - totalcost;
-			System.out.println("\n잔돈은 " + change + "원입니다." );
+		
+		//for문: 음료수 개수에 따라 텅! 반복출력
+		for (int i = 0; i < drinknumber; i++) {
+			System.out.print("텅!");
 		}
+		//잔돈 출력
+		change = money + extraMoney - totalcost;
+		System.out.println("\n잔돈은 " + change + "원입니다." );
 		keyboard.close();
 		
 	}
